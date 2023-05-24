7340beb30cceef1a28cdd0401aaad5dd26163e97
adityapermono8 <adityapermono8@gmail.com>
2023-05-24 18:53:21 +0700

    menambahkan akses modifier public dan constructor dan desctructor

--- memberinitlist/memberinitlist.cpp
+++ memberinitlist/memberinitlist.cpp
@@ -6,6 +6,16 @@ class mahasiswa {
 private:
 	const int id;
 	string nama;
-	float nilai;
+	float nilai;
 
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Member Initialization List 
+		//Definisi
+	}
+
+	~mahasiswa() {
+		cout << "Id " << id << endl;
+		cout << "Nama " << nama << endl;
+		cout << "Nilai " << nilai << endl;
+	}
 };
