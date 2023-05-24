180785d2fdf520e516d6e0d4ed8ecf22aee94b8c
adityapermono8 <adityapermono8@gmail.com>
2023-05-24 15:49:51 +0700

    menambahkan construktor doker dan destruktor dokter dan metod tambah pasien. metod cetak pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -21,3 +21,14 @@ class dokter {
 public:
 	string nama;
 	vector<pasien*> daftar_pasien;
+
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama << "\" tidak ada\n";
+	}
+	void tambahPasien(pasien*);
+	void cetakPasien();
+};
+
